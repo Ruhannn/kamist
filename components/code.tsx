bbf195201a8d544bc1f2bf32008bc64ca3ab2082
Ruhannn <ruhanrouf17@gmail.com>
2025-11-23 07:10:49 +0600

    fixed shikki

--- components/code.tsx
+++ components/code.tsx
@@ -1,26 +1,26 @@
 import { highlight } from "@/lib/shiki";
-import { BundledLanguage } from "shiki/bundle/web";
+
 interface CodeProps {
   code: string;
-  lang: BundledLanguage | string;
+  lang: string;
 }
 
 export default async function Code({ code, lang }: CodeProps) {
   const html = await highlight(code, lang);
-  
 
   return (
     <div className="w-full group relative font-[var(--font-code)]">
-      <div
+      <pre
         id="code"
         className={
-          "w-full overflow-x-auto rounded-xl p-4 bg-[#eff1f5] dark:bg-[#1e1e2e]"
+          "w-full overflow-x-auto rounded-xl p-4 bg-[#eff1f5] dark:bg-[#1e1e2e] hljs"
         }
-        dangerouslySetInnerHTML={{ __html: html }}
         // initial={{ opacity: 0, y: 50 }}
         // animate={{ opacity: 1, y: 0 }}
         // transition={{ duration: 0.5 }}
-      />
+      >
+        <code dangerouslySetInnerHTML={{ __html: html }}></code>
+      </pre>
     </div>
   );
 }
--- lib/shiki.ts
+++ lib/shiki.ts
@@ -1,27 +1,45 @@
-import {
-  bundledLanguages,
-  getSingletonHighlighter,
-  type BundledLanguage,
-} from "shiki";
+import { codeToHtml } from "shiki";
+// import {
+//   bundledLanguages,
+//   getSingletonHighlighter,
+//   type BundledLanguage,
+// } from "shiki";
 
-const highlighter = await getSingletonHighlighter({
-themes: [
-import('shiki/themes/catppuccin-mocha.mjs'),
-import('shiki/themes/catppuccin-latte.mjs'),
-],
-langs: Object.keys(bundledLanguages),
-})
+// const highlighter = await getSingletonHighlighter({
+// themes: [
+// import('shiki/themes/catppuccin-mocha.mjs'),
+// import('shiki/themes/catppuccin-latte.mjs'),
+// ],
+// langs: Object.keys(bundledLanguages),
+// })
 
 export async function highlight(code: string, lang: string) {
-  if (!highlighter.getLoadedLanguages().includes(lang as BundledLanguage)) {
-    lang = "text";
-  }
+  // if (!highlighter.getLoadedLanguages().includes(lang as BundledLanguage)) {
+  //   lang = "text";
+  // }
 
-  return highlighter.codeToHtml(code, {
-    lang,
+  // return highlighter.codeToHtml(code, {
+  //   lang,
+  //   themes: {
+  //     light: "catppuccin-latte",
+  //     dark: "catppuccin-mocha",
+  //   },
+  // });
+  return await codeToHtml(code, {
+    lang: lang,
     themes: {
       light: "catppuccin-latte",
       dark: "catppuccin-mocha",
     },
   });
 }
+// export async function highlight(code: string, lang: string) {
+//   try {
+//     if (hljs.getLanguage(lang)) {
+//       return hljs.highlight(code, { language: lang }).value;
+//     }
+//   } catch (e) {
+//   }
+
+//   return hljs.highlightAuto(code).value;
+// }
